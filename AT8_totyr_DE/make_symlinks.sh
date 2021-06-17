f1a13f2d291c8a7edad105d192d144a08f3c56d8
Filisia Agus <fiagus@scc-ka1.scc.bu.edu>
2021-06-17 17:10:51 -0400

    edited notebooks

--- AT8_totyr_DE/make_symlinks.sh
+++ AT8_totyr_DE/make_symlinks.sh
@@ -19,6 +19,16 @@ ln -s /projectnb/bubhub/users/fiagus/cte_totyrs_at8/jupyter_nb/all_FGSEA_GO_AT8_
 ln -s /projectnb/bubhub/users/fiagus/cte_totyrs_at8/samples/HTSeq_DE/CTES_RHIN_all_counts_filt.csv
 ln -s /projectnb/bubhub/users/fiagus/cte_totyrs_at8/samples/HTSeq_DE/CTEM_RHIN_all_counts_filt.csv
 
+# AT8 Log
+for gz in /projectnb/bubhub/users/fiagus/cte_totyrs_at8/samples/HTSeq_DE/C*all*AT8Log*annot*; do
+    ln -s $gz ${gz##*/}
+done
+
+# Abeta
+for gz in /projectnb/bubhub/users/fiagus/cte_totyrs_at8/samples/HTSeq_DE/*Abeta*annot*; do
+    ln -s $gz ${gz##*/}
+done
+
 # QPCR
 ln -s /projectnb/bubhub/users/fiagus/cte_totyrs_at8/jupyter_nb/qpcr_ALL_linear_reg.csv
 ln -s /projectnb/bubhub/users/fiagus/cte_totyrs_at8/samples/HTSeq_DE/all_DE_AT8Log_combined_annot.csv
